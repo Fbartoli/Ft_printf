2269b3da24f8cbe6991f8a114330af19c97a636f
Apolline SAINT-MLEUX <apsaint-@e3r7p20.42.fr>
2019-01-25 15:47:15 +0100

    edit

--- srcs/conversions_csp.c
+++ srcs/conversions_csp.c
@@ -6,7 +6,7 @@
 /*   By: apsaint- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 14:09:23 by apsaint-          #+#    #+#             */
-/*   Updated: 2019/01/25 15:44:18 by apsaint-         ###   ########.fr       */
+/*   Updated: 2019/01/25 15:46:18 by apsaint-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		conv_c(va_list *params, t_flag struc)
 {
 	wint_t	c;
 
-	if (struc->taille[0] == 'l' || struc->conv == 'C')
+	if (struc.taille[0] == 'l' || struc.conv == 'C')
 		c = (wchar_t)va_arg(*params, wint_t);
 	else
 		c = (char)va_arg(*params, int);
@@ -30,7 +30,7 @@ void	*conv_s(va_list *params, t_flag struc)
 {
 	void	*s;
 
-	if (struc->taille[0] == 'l' || struc->conv == 'S')
+	if (struc.taille[0] == 'l' || struc.conv == 'S')
 		s = (wchar_t *)va_arg(*params, wchar_t *);
 	else
 		s = (char *)va_arg(*params, char *);
--- srcs/print_args.c
+++ srcs/print_args.c
@@ -6,7 +6,7 @@
 /*   By: apsaint- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/25 14:05:07 by apsaint-          #+#    #+#             */
-/*   Updated: 2019/01/25 15:44:51 by apsaint-         ###   ########.fr       */
+/*   Updated: 2019/01/25 15:46:53 by apsaint-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ void	ft_print_c(char c, t_flag struc)
 {
 	int pad;
 
-	pad = struc->min - 1;
-	if (struc->flag == '-')
+	pad = struc.min - 1;
+	if (struc.flag == '-')
 		ft_putchar(c);
 	while (pad--)
 		ft_putchar(' ');
-	if (struc->flag == '\0')
+	if (struc.flag == '\0')
 		ft_putchar(c);
 }
 
@@ -29,11 +29,11 @@ void	ft_print_str(char *str, t_flag struc)
 {
 	int pad;
 
-	pad = struc->min - ft_strlen(str);
-	if (struc->flag == '-')
+	pad = struc.min - ft_strlen(str);
+	if (struc.flag == '-')
 		ft_putstr(str);
 	while (pad--)
 		ft_putchar(' ');
-	if (struc->flag == '\0')
+	if (struc.flag == '\0')
 		ft_putstr(str);
 }
