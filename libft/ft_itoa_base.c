eba131e2d987622f19f495b49dc13539521cc725
Fbartoli <fbartoli@monaco.edu>
2019-02-01 14:38:49 +0100

    itoa base positif

--- libft/ft_itoa_base.c
+++ libft/ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: flbartol <flbartol@42.student.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/11 18:16:34 by flbartol          #+#    #+#             */
-/*   Updated: 2019/02/01 11:20:33 by flbartol         ###   ########.fr       */
+/*   Updated: 2019/02/01 14:38:32 by flbartol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,6 @@ char	*ft_itoa_base(unsigned long value, int base)
 
 	i = 1;
 	neg = 0;
-	if (value < 0)
-	{
-		if (base == 10)
-			neg = 1;
-		value *= -1;
-	}
 	while (ft_pow(base, i) - 1 < value)
 		i++;
 	nbr = (char*)malloc(sizeof(nbr) * i);
@@ -35,7 +29,5 @@ char	*ft_itoa_base(unsigned long value, int base)
 		nbr[i + neg] = (value % base) + (value % base > 9 ? 'A' - 10 : '0');
 		value = value / base;
 	}
-	if (neg)
-		nbr[0] = '-';
 	return (nbr);
 }
