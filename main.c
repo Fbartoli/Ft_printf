d67e84c124f4ddf914c862483104abc120139054
Apolline SAINT-MLEUX <apsaint-@e2r6p16.42.fr>
2019-02-04 14:32:20 +0100

    gestion precision s

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: flbartol <flbartol@42.student.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/23 13:34:51 by flbartol          #+#    #+#             */
-/*   Updated: 2019/02/03 20:38:03 by flbartol         ###   ########.fr       */
+/*   Updated: 2019/02/04 14:09:18 by apsaint-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,18 @@
 
 int		main()
 {
-	printf("@moulitest: %e\n", 1204.5);
-	ft_printf("%0+10.3f!\n", 14.123456789);
-	printf("%0+10.3f!\n", 14.123456789);
+	//printf("@moulitest: %e\n", 1204.5);
+//	ft_printf("ft_printf : %0+10.3f!\n", 1400000000000000000000000000.123456789);
+//	printf("printf: %0+10.3f!\n", 1400000000000000000000000000.123456789);
+	//ft_printf("my printf: %.4s\n", "42");
+	//printf("le printf: %.4s\n", "42");
+	//ft_printf("my printf: %.4s\n", "42 is the answer");
+	//printf("le printf: %.4s\n", "42 is the answer");
+//printf("printf : %lu\n", -42);
+	//ft_printf("%s\n", "this is a string");
+	ft_printf("my printf: %4.s\n", "42");
+	ft_printf("my printf: %4.d\n", 42);
+	printf("le printf: %4.s\n", "42");
+	printf("le printf: %4.d\n", 42);
 	return (1);
 }
