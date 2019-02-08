185dcd559306a6d5a4b10227a02bdbec87bc4dad
Florent BARTOLI <flbartol@e2r8p20.42.fr>
2019-02-08 18:00:30 +0100

    fix O and hex

--- srcs/print_o.c
+++ srcs/print_o.c
@@ -6,7 +6,7 @@
 /*   By: flbartol <flbartol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/01 11:10:05 by flbartol          #+#    #+#             */
-/*   Updated: 2019/02/08 14:19:46 by flbartol         ###   ########.fr       */
+/*   Updated: 2019/02/08 16:45:17 by flbartol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,19 @@
 
 static	int				ft_print_o_null(t_flag *struc)
 {
-	if (struc->prec_default == 1 || (struc->prec_default != 1
-		&& struc->prec != 0))
+	if ((struc->prec_default == 1 || (struc->prec_default == 0 && struc->prec != 0)))
 		struc->pad = struc->min - struc->prec - 1;
 	else
 		struc->pad = struc->min - struc->prec;
+	if (struc->force_prefix && struc->prec > 0)
+	{
+		struc->prec--;
+		struc->pad++;
+	}
 	if (struc->right_pad == 0)
 		padding(struc);
-	if ((struc->prec_default == 1 && struc->min == 0) || struc->min != 0)
+	if ((struc->prec_default == 1) || (struc->min != 0 && struc->prec != 0)
+		|| struc->force_prefix == 1)
 		struc->i += ft_putnbr(0);
 	if (struc->right_pad == 1)
 			padding(struc);
@@ -32,6 +37,8 @@ static int		end_o(char *str, int count, t_flag *struc)
 {
 	if (struc->prec != 0 && (size_t)struc->prec > ft_strlen(str))
 		count += struc->prec - ft_strlen(str);
+	if (struc->prec != 0 && (size_t)struc->prec == ft_strlen(str))
+		count++;
 	if (struc->force_prefix && str[0] != '0' &&
 		struc->prec < (int)ft_strlen(str))
 		count++;
@@ -46,7 +53,7 @@ static	int		check_complet_charo(char *nb, char letter, t_flag *struc)
 	int	tmp;
 	int count;
 
-	if (ft_strlen(nb) < (size_t)(struc->min))
+	if ( (size_t)(struc->min) > ft_strlen(nb))
 	{
 		if ((struc->prec != 0) && ((size_t)struc->prec > ft_strlen(nb)))
 			tmp = struc->min - struc->prec;
@@ -69,7 +76,7 @@ static	int		check_complet_charo(char *nb, char letter, t_flag *struc)
 static void		putzero_ifsharp(char *nb, t_flag *struc)
 {
 	if (struc->force_prefix && !(ft_strlen(nb) == 1 && nb[0] == '0') &&
-		struc->prec < (int)ft_strlen(nb))
+		struc->prec <= (int)ft_strlen(nb))
 		ft_putchar('0');
 }
 
@@ -83,7 +90,8 @@ static int		with_mino(char *nb, t_flag *struc)
 		ft_puto(nb, struc);
 		count = check_complet_charo(nb, ' ', struc);
 	}
-	else if (struc->pad_zeroes)
+	else if (struc->pad_zeroes && (struc->prec_default
+			|| (struc->prec > struc->min)))
 	{
 		count = check_complet_charo(nb, '0', struc);
 		putzero_ifsharp(nb, struc);
@@ -103,7 +111,7 @@ int				ft_print_o(unsigned long nb, t_flag *struc)
 	char	*str;
 	int		count;
 
-	if (!nb && !struc->force_prefix)
+	if (!nb)
 		return (ft_print_o_null(struc));
 	str = ft_itoa_base(nb, 8);
 	if (struc->min)
