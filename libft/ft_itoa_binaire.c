135aa04ea34e88db7aec1bbefd6f834b7e9c7c14
Apolline SAINT-MLEUX <apsaint-@e2r8p18.42.fr>
2019-02-08 14:07:38 +0100

    gestion binaire negatif

--- libft/ft_itoa_binaire.c
+++ libft/ft_itoa_binaire.c
@@ -1,23 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   conversions_b.c                                    :+:      :+:    :+:   */
+/*   ft_itoa_binaire.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apsaint- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/08 09:13:33 by apsaint-          #+#    #+#             */
-/*   Updated: 2019/02/08 13:57:27 by apsaint-         ###   ########.fr       */
+/*   Created: 2019/02/08 11:44:54 by apsaint-          #+#    #+#             */
+/*   Updated: 2019/02/08 13:56:58 by apsaint-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "libft.h"
 
-char		*conv_b(va_list *params)
+char	*ft_itoa_binaire(long value)
 {
-	long	nbr;
+	int		i;
+	int		neg;
 	char	*bin;
 
-	nbr = va_arg(*params, int);
-	bin = ft_itoa_binaire(nbr);
+	i = 1;
+	if (value < 0)
+	{
+		neg = 1;
+		value = -value;
+	}
+	else
+		neg = 0;
+	while (ft_pow(2, i) - 1 < (unsigned long)value)
+		i++;
+	bin = (char *)malloc(sizeof(bin) * (i + 1));
+	bin[i++] = '\0';
+	bin[0] = neg + '0';
+	while (--i > 0)
+	{
+		bin[i] = (value % 2) + (value % 2 > 9 ? 'A' - 10 : '0');
+		value = value / 2;
+	}
 	return (bin);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: flbartol <flbartol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/23 13:34:51 by flbartol          #+#    #+#             */
-/*   Updated: 2019/02/08 10:58:40 by apsaint-         ###   ########.fr       */
+/*   Updated: 2019/02/08 13:53:11 by apsaint-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 int		main()
 {
 	//ft_printf("My printf: %15.4S\n", L"我是一只猫。");
-	ft_printf("My printf: %-10b\n", 2);
+	ft_printf("My printf: %-10b\n", -2);
 	ft_printf("My printf: %-10b\n", 300);
+	ft_printf("%s\n", ft_itoa_base(300, 2));
 	//ft_printf("Le printf: {%05.S}", L"42 c est cool");
 	return (1);
 }
